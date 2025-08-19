cbc885f4aedc7d549c3669a50d76e0189d2214ca
sund1 <33967850+sund1@users.noreply.github.com>
2025-08-20 02:36:31 +0300

    Update flu.cpp
    asdfasdfa

--- src/jrd/flu.cpp
+++ src/jrd/flu.cpp
@@ -64,7 +64,7 @@
 
 #include <string.h>
 
-#if (defined SOLARIS || defined LINUX || defined DARWIN || defined AIX_PPC || defined FREEBSD || defined NETBSD || defined HPUX)
+#if (defined SOLARIS || defined LINUX || defined AIX_PPC || defined FREEBSD || defined NETBSD || defined HPUX)
 #define DYNAMIC_SHARED_LIBRARIES
 #endif
 
@@ -113,11 +113,11 @@ namespace {
 		{MOD_SUFFIX, "." SHRLIB_EXT, true},
 		{MOD_PREFIX, "lib", true},
 #endif
-/*
+
 #ifdef DARWIN
 		{MOD_SUFFIX, ".dylib", true},
 #endif
-*/
+
 	};
 
 	// UDF/BLOB filter verifier
@@ -126,23 +126,13 @@ namespace {
 	private:
 		const Firebird::PathName getConfigString() const
 		{
-			const char* udfAccess = Config::getUdfAccess();
-	
-	        gds__log("[UDF CONFIG] Config string = %s\n", udfAccess);
-	
-	        return Firebird::PathName(udfAccess);
+			return Firebird::PathName(Config::getUdfAccess());
 		}
 	public:
 		explicit UdfDirectoryList(MemoryPool& p)
 			: DirectoryList(p)
 		{
 			initialize();
-
-			for (const auto& dir : *this)
-			{
-				PathName path = dir;  
-				gds__log("[UDF DIR] %s\n", path.c_str());
-			}
 		}
 
 		~UdfDirectoryList()
@@ -207,9 +197,6 @@ namespace Jrd
 		{
 			return Module(im);
 		}
-		
-		gds__log("[UDF SCAN] Start scanning module '%s', total variants: %zu\n",
-         initialModule.c_str(), sizeof(libfixes) / sizeof(Libfix));
 
 		// apply suffix (and/or prefix) and try that name
 		Firebird::PathName module(initialModule);
@@ -218,12 +205,6 @@ namespace Jrd
 			const Libfix* l = &libfixes[i];
 			// os-dependent module name modification
 			Firebird::PathName fixedModule(module);
-
-			gds__log("[UDF LOOP START] Iteration %zu, kind = %s, base module = %s\n",
-             i,
-             l->kind == MOD_PREFIX ? "PREFIX" : "SUFFIX",
-             module.c_str());
-
 			switch (l->kind)
 			{
 			case MOD_PREFIX:
@@ -237,8 +218,6 @@ namespace Jrd
 				module = fixedModule;
 			}
 
-		    gds__log("[UDF SCAN] Variant %zu: %s\n", i, fixedModule.c_str());
-
 			// Look for module with fixed name
 			im = scanModule(fixedModule);
 			if (im)
@@ -246,45 +225,21 @@ namespace Jrd
 				return Module(im);
 			}
 
-			gds__log("[UDF SCAN] Variant %zu: %s\n", i, fixedModule.c_str());
-
 			// UdfAccess verification
 			Firebird::PathName path, relative;
 
 			// Search for module name in UdfAccess restricted
 			// paths list
 			PathUtils::splitLastComponent(path, relative, fixedModule);
-
-			gds__log("[UDF DEBUG] fixedModule = %s, path = %s, relative = %s\n",
-			fixedModule.c_str(),
-			path.c_str(),
-			relative.c_str());
-
 			if (path.isEmpty() && PathUtils::isRelative(fixedModule))
 			{
 				path = fixedModule;
-
-				const auto& udfList = iUdfDirectoryList();
-				gds__log("[UDF DEBUG] Allowed directories (%zu):\n", udfList.getCount());
-				for (FB_SIZE_T i = 0; i < udfList.getCount(); ++i)
-				{
-				    gds__log("  %s\n", PathName(udfList[i]).c_str());
-				}
-
-				gds__log("[UDF DEBUG] Trying to expand relative module path: %s\n", path.c_str());
-
-				
 				if (! iUdfDirectoryList().expandFileName(fixedModule, path))
 				{
-			        gds__log("[UDF DEBUG] expandFileName FAILED for: %s\n", path.c_str());
-
-
 					// relative path was used, but no appropriate file present
 					continue;
 				}
 			}
-
-			gds__log("[UDF CHECK] fixedModule = %s\n", fixedModule.c_str());
 
 			// The module name, including directory path,
 			// must satisfy UdfAccess entry in config file.
@@ -292,22 +247,9 @@ namespace Jrd
 			{
 				ERR_post(Arg::Gds(isc_conf_access_denied) << Arg::Str("UDF/BLOB-filter module") <<
 															 Arg::Str(initialModule));
-
-				gds__log("[UDF DEBUG] isPathInList(%s) = %d\n", fixedModule.c_str(),
-         			iUdfDirectoryList().isPathInList(fixedModule));
 			}
 
 			ModuleLoader::Module* mlm = ModuleLoader::loadModule(NULL, fixedModule);
-
-			if (mlm)
-			{
-				gds__log("[UDF LOAD] SUCCESS: %s\n", fixedModule.c_str());
-			}
-			else
-			{
-				gds__log("[UDF LOAD] FAILED: %s\n", fixedModule.c_str());
-			}
-
 			if (mlm)
 			{
 				im = FB_NEW_POOL(*getDefaultMemoryPool())
